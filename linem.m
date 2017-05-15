574eb1f82f8d4436772aa134a65a14b3715db68e
Sky Sartorius <Sky.Sartorius@gmail.com>
2017-05-15 09:21:33 -0700

    Initial commit

---
+++ linem.m
@@ -0,0 +1,30 @@
+function h = linem(varargin)
+% linem  Line using single 2 x n (or 3 x n) matrix as input instead of x, y,
+% (and z) vectors. linem(M) is equivalent to line(M(1,:),M(2,:)) or
+% line(M(1,:),M(2,:),M(3,:)).
+% 
+%   Scalars and row vectors (including handles, Name/Value pairs, etc.) are
+%   preserved and passed to the line function.
+% 
+%   Example.
+%     linem(randn(3,100),'LineStyle','none','Marker','x')
+% 
+%   See also plot, plot3, line, plotm, transpose.
+
+for i = numel(varargin):-1:1
+    M = varargin{i};
+    if size(M,1) >= 2
+        nDims = size(M,1);
+        if nDims > 3
+            error('Input must have no more than 3 rows.')
+        end
+        M = mat2cell(M,ones(nDims,1));
+        varargin = [varargin(1:i-1) {M{:}} varargin(i+1:end)];
+    end
+end
+
+h_ = line(varargin{:});
+
+if nargout
+    h = h_;
+end
---
+++ plotm.m
@@ -0,0 +1,44 @@
+function h = plotm(varargin)
+% plotm  Plot using single 2 x n (or 3 x n) matrices as inputs instead of x, y,
+% (and z) vectors. plotm(M) is equivalent to plot(M(1,:),M(2,:)) or
+% plot3(M(1,:),M(2,:),M(3,:)).
+% 
+%   plotm works by separating any matrix with two or more rows into vectors. The
+%   vectors are passed to plot for 2 x n and to plot3 for 3 x n. If no inputs
+%   have multiple rows, plotm calls plot.
+% 
+%   Scalars and row vectors (including handles, Name/Value pairs, LineSpecs,
+%   etc.) are preserved and passed to the plotting functions.
+% 
+%   Mixed inputs with two rows and three rows will return an error. 
+% 
+%   Example: Plot using matrices.
+%     M1 = randn(3,100);
+%     M2 = M1.^2;
+%     plotm(M1,'ob',M2,'xr')
+% 
+%   See also plot, plot3, line, linem, transpose.
+
+nDims = NaN;
+
+for i = numel(varargin):-1:1
+    M = varargin{i};
+    if size(M,1) >= 2
+        nDims = size(M,1);
+        if nDims > 3
+            error('Input must have no more than 3 rows.')
+        end
+        M = mat2cell(M,ones(nDims,1));
+        varargin = [varargin(1:i-1) {M{:}} varargin(i+1:end)];
+    end
+end
+
+if nDims == 3
+    h_ = plot3(varargin{:});
+else
+    h_ = plot(varargin{:});
+end
+
+if nargout
+    h = h_;
+end
